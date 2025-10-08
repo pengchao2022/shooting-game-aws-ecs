289c4473e1b2adab915200c9687d964a1398d41b
pengchao <pengchao.ma2@outlook.com>
2025-10-08 09:47:59 +0800

    modified

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -26,7 +26,7 @@ resource "aws_ecs_task_definition" "backend" {
   family                   = "${var.project_name}-backend"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
+  cpu                      = "1024"
   memory                   = "2"
   execution_role_arn       = var.ecs_task_execution_role_arn
 
@@ -67,8 +67,8 @@ resource "aws_ecs_task_definition" "frontend" {
   family                   = "${var.project_name}-frontend"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "512"
-  memory                   = "2"
+  cpu                      = "256"
+  memory                   = "512"
   execution_role_arn       = var.ecs_task_execution_role_arn
 
   container_definitions = jsonencode([{
